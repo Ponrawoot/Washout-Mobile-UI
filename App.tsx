b1d8378b4f1f0099397ceef0e935cbfe316ba60f
Ponrawoot <minnaja853@gmail.com>
2023-11-01 09:58:54 +0700

    test noti

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+  React.useEffect(() => {
+    (async () => {
+      const { status } = await Notifications.requestPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Permission to send notifications has been denied.');
+        return;
+      }
+    })();
+
+    const subscription = Notifications.addNotificationReceivedListener((notification) => {
+      console.log('Notification received:', notification);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const sendNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Test Notification',
+        body: 'Washout.',
+      },
+      trigger: null,
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Test Noti</Text>
+      <Button onPress={sendNotification} title="Mock Noti" color="#841584" accessibilityLabel="purple button" />
     </View>
   );
 }
